82238d196da8a04d9ed00c8d38770fae848c8a09
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-24 17:38:07 +0000

    Initial commit - Bobby Tales codeCademy project.

---
+++ app.php
@@ -0,0 +1,53 @@
+<?php
+$name = "";
+$character = "";
+$email = "";
+$birth_year = 1969;
+$validation_error = "";
+$existing_users = ["admin", "guest"];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST["name"];
+  $character = $_POST["character"];
+  $email = $_POST["email"];
+  $birth_year = $_POST["birth_year"];
+}
+?>
+<h1>Create your profile</h1>
+<form method="post" action="">
+<p>
+Select a name: <input type="text" name="name" value="<?php echo $name;?>" >
+</p>
+<p>
+Select a character:
+  <input type="radio" name="character" value="wizard" <?php echo ($character=='wizard')?'checked':'' ?>> Wizard
+  <input type="radio" name="character" value="mage" <?php echo ($character=='mage')?'checked':'' ?>> Mage
+  <input type="radio" name="character" value="orc" <?php echo ($character=='orc')?'checked':'' ?>> Orc
+</p>
+<p>
+Enter an email:
+<input type="text" name="email" value="<?php echo $email;?>" >
+</p>
+<p>
+Enter your birth year:
+<input type="text" name="birth_year" value="<?php echo $birth_year;?>">
+</p>
+<p>
+  <span style="color:red;"><?= $validation_error;?></span>
+</p>
+<input type="submit" value="Submit">
+</form>
+  
+<h2>Preview:</h2>
+<p>
+  Name: <?=$name;?>
+</p>
+<p>
+  Character Type: <?=$character;?>
+</p>
+<p>
+  Email: <?=$email;?>
+</p>
+<p>
+  Age: <?=date("Y")-$birth_year;?>
+</p>
